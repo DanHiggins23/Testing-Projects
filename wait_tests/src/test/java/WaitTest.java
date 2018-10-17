9517e670a2751d0a1d2a848b2e16cf561140568d
DanHiggins23 <danhiggins23@outlook.com>
2018-10-17 20:23:25 +0100

    changed report download location to downloads

--- wait_tests/src/test/java/WaitTest.java
+++ wait_tests/src/test/java/WaitTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
-
 import static org.junit.Assert.assertEquals;
 
 public class WaitTest {
